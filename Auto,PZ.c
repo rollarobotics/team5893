5bf6f897789e052281e52ff1669b03de41d90f7f
RollaRoboticsTeam5893 <aaronburke26@gmail.com>
2014-10-07 19:20:45 -0500

    Autonomous skeleton programs

---
+++ Auto,PZ.c
@@ -0,0 +1,31 @@
+/*
+
+Autonomous Parking Zone Plan:
+
+1. Set up Robot for competition.
+
+2. Drive towards opposing alliance's Ramp Zone using gyroscopic sensor and motor encoders.
+		b. Drive close enough to not enter Ramp Zone, but far enough to not hit the kickstand.
+
+3. Drive towards opposing alliance's Parking Zone using gyroscopic sensor and motor encoders.
+		b. Drive close enough to not enter Parking Zone, but far enough to not hit the kickstand.
+
+4. Drive to 30cm rolling goal using gyroscopic sensor and motor encoders, attach to rolling goal/Score.
+
+5. Take IR reading
+
+6. Drive to Parking Zone using gyroscopic sensor and moter encoders.
+
+7. Release 30cm rolling goal.
+
+8. Drive to/Kick Kickstand using gyroscopic sensor and motor encoders.
+
+9. Drive to center goal using gyroscopic sensor and motor encoders, and score.
+
+*/
+task main()
+{
+
+
+
+}
---
+++ Auto,Ramp.c
@@ -0,0 +1,26 @@
+/*
+
+Autonomous Ramp Plan:
+
+1.Set up Robot for competition.
+
+2.Drive down ramp using gyroscopic sensor and motor encoders.
+
+3.Take IR reading/Score in center goal using gyroscopic sensor and motor encoders.
+
+3.Drive to 60cm rolling goal/Attach/Score using gyroscopic sensor and moter encoders.
+
+4.Drive to Parking Zone using gyroscopic sensor and motor encoders.
+
+5.Release 60 cm rolling goal in Parking Zone.
+
+6.Drive to and knock over Kickstand, using gyroscopic sensor and motor encoders based upon previous IR reading.
+
+*/
+
+task main()
+{
+
+
+
+}
